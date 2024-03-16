59430f4ff693b839e0eae930ed28e72344488968
Sudhanshu Joshi <72558573+SudhanshuJoshi09@users.noreply.github.com>
2024-03-16 00:52:46 +0000

    chore: updated .gitignore

--- 01-trie/208. Implement Trie (Prefix Tree)/haskell/src/Main.hs
+++ 01-trie/208. Implement Trie (Prefix Tree)/haskell/src/Main.hs
@@ -1,22 +1,61 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Main where
 
 import qualified Data.Map.Strict as M
 
-data Trie = TrieNode String (M.Map Char Trie) | TrieLeaf String deriving (Eq, Show)
+data Trie = Trie
+  { path :: String
+  , links :: M.Map Char Trie
+  } deriving (Eq, Show)
 
-buildTrie :: [Char] -> Trie
-buildTrie (curr:suffix) =
+buildTrie :: [Char] -> [Char] -> Trie
+buildTrie [curr] prefix =
+  Trie
+  { path = prefix ++ [curr]
+  , links = M.empty
+  }
+buildTrie [] prefix = 
+  Trie
+  { path = prefix
+  , links = M.empty
+  }
+buildTrie (curr:suffix) prefix =
+  let suffixTrie = buildTrie suffix $ prefix ++ [curr]
+  in Trie
+  { path = prefix ++ [curr]
+  , links = M.insert curr suffixTrie M.empty
+  }
 
-insert :: [Char] -> Trie -> String -> Trie
+insert :: [Char] -> Trie -> [Char] -> Trie
 insert [] trie _ = trie
 insert (curr:suffix) trie prefix =
-  case trie of
-    TrieNode triePrefix childrenMapping ->
-    TrieLeaf leafPrefix ->
+  case (M.lookup curr links) of
+    Just nextTrieVal -> insert suffix nextTrieVal $ prefix ++ [curr]
+    Nothing ->
+      let buildTrieVal = buildTrie (curr:suffix) (currPath)
+          updatedLinks = M.insert curr buildTrieVal links
+      in trie {
+        links = updatedLinks
+      }
+  where
+    links = getLinks trie
+    currPath = getPath trie
+  
+getLinks :: Trie -> M.Map Char Trie
+getLinks = links
+
+getPath :: Trie -> String
+getPath = path
 
 main :: IO ()
 main = do
-  print $ newTrie
-  putStrLn "Hello, Haskell!"
+  print $ newTrie2
   where
-    newTrie = TrieLeaf $ T.pack "Something"
+    newTrie =
+      Trie
+        { path = ""
+        , links = M.empty
+        }
+
+    newTrie1 = insert "Hello, World" newTrie ""
+    newTrie2 = insert "Hello, Haskell" newTrie ""
